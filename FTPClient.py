986c6433e6d17dbb7bb24e9e6633b89b616b3575
Frankjer1 <fr.bernaudo@gmail.com>
2024-06-21 22:45:14 +0200

    aggiornamento gestione errori

---
+++ FTPClient.py
@@ -0,0 +1,118 @@
+import socket as s
+import os
+#N.B. SI SONO TRALASCIATE NELLA DESCRIZIONE DELLE FUNZIONI LA GESTIONE DELLE ECCEZIONI E I METODI DI CHECK DEI VARI COMANDI
+
+
+#Script FTP Client
+def FTPClientScript():
+
+    #INDIRIZZO SERVER
+    HOST = '127.0.0.1'
+    # PORTA PI SERVER PER I COMANDI
+    CONTROL_SERVER_PORT = 2121
+
+
+    try:
+
+        while True:
+            #CREAZIONE SOCKET PER PI E CONNESSIONE
+            control_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+            tryToConnect(control_socket, HOST, CONTROL_SERVER_PORT)
+            #RISPOSTA ALLA CONNESSIONE SOCKET
+            print(control_socket.recv(1024).decode())
+
+            #INSERIMENTO CREDENZIALI
+            print(control_socket.recv(1024).decode())
+            username = input("Username: ")
+            control_socket.send(f"{username}".encode())
+
+            if username != "anonymous":
+                print(control_socket.recv(1024).decode())
+                password = input("Password: ")
+                control_socket.send(f"{password}".encode())
+            
+            response = control_socket.recv(1024).decode() 
+            print(response)
+            if(response == "230 Welcome, anonymous user. Your root directory is \"public\"\r\n" or response == "230 User logged in, proceed. your root directory is \"documents\"\r\n"):
+                break
+
+
+        while True:
+            #ACQUISIZIONE COMANDO DA INPUT
+            command = input("Enter FTP command (e.g., LIST, RETR filename, QUIT): ").strip().upper()
+            #INVIO COMANDO AL SERVER
+            control_socket.send(command.encode())
+            if command.startswith("LIST"):
+                control_response = control_socket.recv(4096).decode()
+                print(control_response)
+                if(control_response == "330 Proceeding with the request...\r\n"):
+                    #SE COMANDO LIST, CREAZIONE DI UN SOCKET PER DTP CON LA PORTA INVIATA DAL SERVER
+                    data_port = int(control_socket.recv(4096).decode('utf-8'))
+                    data_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+                    data_socket.connect((HOST, data_port))
+                    data_response = data_socket.recv(4096).decode()
+                    print(data_response)
+                    control_response = control_socket.recv(4096).decode()
+                    print(control_response)
+                    data_socket.close()
+            elif command == "QUIT":
+                break
+            elif command.startswith('RETR'):
+                control_response = control_socket.recv(4096).decode()
+                print(control_response)
+                if(control_response == "330 Proceeding with the request...\r\n"):
+                #SE COMANDO RETR, CREAZIONE DI UN SOCKET PER DTP CON LA PORTA INVIATA DAL SERVER
+                # E SCRITTURA DEI DATI RICEVUTI DAL FILE RICHIESTO SU UN NUOVO FILE
+                    data_port = int(control_socket.recv(4096).decode('utf-8'))
+                    data_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+                    data_socket.connect((HOST, data_port))
+                    with open(os.path.abspath(command.split(" ")[1].lower()), 'wb') as file:
+                        data = data_socket.recv(1024)
+                        while data:
+                            file.write(data)
+                            data = data_socket.recv(1024)
+                    data_socket.close()
+                    response = control_socket.recv(4096).decode()
+                    print(response)
+                    print(f"File '{command.split(" ")[1]}' downloaded correctly.")
+            elif command.startswith('STOR'):
+                control_response = control_socket.recv(4096).decode()
+                print(control_response)
+                if(control_response == "330 Proceeding with the request...\r\n"):
+                    #SE COMANDO STOR, CREAZIONE DI UN SOCKET PER DTP CON LA PORTA INVIATA DAL SERVER
+                    # E INVIO DEI DATI DEL FILE SELEZIONATO AL SERVER
+                    data_port = int(control_socket.recv(1024).decode('utf-8'))
+                    data_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+                    data_socket.connect((HOST, data_port))
+                    with open(command.split(" ")[1].lower(), 'rb') as file:
+                        f = file.read()
+                        data_socket.sendall(f)
+                    data_socket.close()
+                    response = control_socket.recv(4096).decode()
+                    print(response)
+            else:
+                control_response = control_socket.recv(4096).decode()
+                print(control_response)
+                if(control_response == "330 Proceeding with the request...\r\n"):
+                    response = control_socket.recv(4096).decode()
+                    print(response)
+
+
+
+    except Exception as e:
+        print("Error", e)
+    finally:
+        # Chiudi la socket di controllo
+        control_socket.close()
+
+
+
+
+def tryToConnect(socket: s.socket, address: str, port: int):
+    try:
+        socket.connect((address, port))
+        print("Connection estabilished")
+    except:
+        print("Connection not estabilished. Impossible to find Server on port: ", port)
+
+FTPClientScript()
---
+++ README.md
@@ -0,0 +1,17 @@
+# ClientServerFTP
+Example of a Client/Server FTP architecture and implementation using python3
+## Prerequisites
+To execute the scripts you must have python3 installed. The program has been tested with The 3.12 version.
+## Dependencies
+The program has just two dependencies: "os" and "socket", already available with the standard library.
+## Instructions
+You must execute the server script first, then the client one:
+
+python FTPServer.py
+
+python FTPClient.py
+
+
+
+
+
